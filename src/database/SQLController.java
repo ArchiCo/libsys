b6c7c02dab60d3ed5e9cd1d85d517bba31f7282b
Ilja Pavlov <guspavloil@student.gu.se>
2017-11-17 02:35:47 +0100

    Split away the ResultSet processing function in SQLController away from the printouts. returnColumns returns the result in an ArrayList of Strings which later gets printed out.

--- src/database/SQLController.java
+++ src/database/SQLController.java
@@ -2,6 +2,8 @@ package database;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SQLController {
 	final int DEBUG_TIMEOUT = 5000;
@@ -37,19 +39,37 @@ public class SQLController {
 	
 	public void printColumnNames(String table) {
 		try {
-			ResultSet rs = mysql.query("SELECT * FROM " + table);
-			if (rs != null) {
-				for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-					System.out.println("Column #" + i + ":" +  
-								        rs.getMetaData().getColumnName(i));
+			List<String> columns = returnColumns(table);
+			if (columns != null) {
+				for (String columnName : columns) {
+					System.out.println(columnName);
 				}
-				rs.close();
 			} else {
 				System.out.println("No result returned.");
 			}
 		} catch (Exception e) {
 			
 		}
+	}
+	
+	
+	public List<String> returnColumns(String table) {
+		try {
+			ResultSet rs = mysql.query("SELECT * FROM " + table);
+			if (rs != null) {
+				List<String> columns = new ArrayList<String>();
+				for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+					columns.add(rs.getMetaData().getColumnName(i));
+				}
+				rs.close();
+				return columns;
+			} else {
+				System.out.println("No result returned.");
+			}
+		} catch (Exception e) {
+			
+		}
+		return null;
 	}
 	
 }
